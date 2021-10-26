d4114bb20bc6edb67ba2cebb1f842313b77bb091
Doruk <doruksen97@gmail.com>
2021-10-26 12:06:25 +0300

    tow_4

---
+++ Assets/Scripts/GameState/LevelManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    #region instance structer
+    public static LevelManager instance;
+    public static GameState gameState;
+    private void Awake()
+    {
+        instance = this;
+    }
+    #endregion
+}
---
+++ Assets/Scripts/Movement/TruckMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TruckMovement : MonoBehaviour
+{
+    #region instance structure
+    public static TruckMovement instance;
+    private void Awake()
+    {
+        instance = this;
+    }
+    #endregion
+    Rigidbody rigidbody;
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+    void GoForward()
+    {
+        rigidbody.AddForce(transform.forward*0.5f);
+    }
+
+    void Update()
+    {
+        if (LevelManager.gameState==GameState.Normal)
+        {
+            GoForward();
+        }
+    }
+    public void Swiper(float xLine)
+    {
+        Vector3 pos = new Vector3(xLine, 0, 0);
+        transform.position += pos * 2;
+        Vector3 clampPos = transform.position;
+        clampPos.x= Mathf.Clamp(clampPos.x,-1.25f,1.25f);
+        transform.position = clampPos;
+    }
+}
