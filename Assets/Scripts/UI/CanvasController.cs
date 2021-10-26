1565db530a3bb3bea1971c35983e0a5171d5c0f5
atakankorkmaz <atakankorkmaz15@gmail.com>
2021-10-26 17:26:25 +0300

    ui_2

---
+++ Assets/Scripts/UI/CanvasController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasController : MonoBehaviour
+{
+    public Canvas canvas;
+    public GameObject options;
+    public GameObject retry;
+    public GameObject tapToStart;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (LevelManager.gameState == GameState.BeforeStart)
+        {
+            options.SetActive(true);
+            retry.SetActive(false);
+            tapToStart.SetActive(true);
+        }
+        if (LevelManager.gameState == GameState.Normal)
+        {
+            options.SetActive(false);
+            retry.SetActive(true);
+            tapToStart.SetActive(false);
+        }
+        if (LevelManager.gameState == GameState.Finish)
+        {
+            print("finish");
+        }
+        if (Input.GetKeyDown("space"))
+        {
+            LevelManager.gameState = GameState.Finish;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/SwitchHandler.cs
@@ -0,0 +1,46 @@
+﻿using System; 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+
+public class SwitchHandler : MonoBehaviour
+{
+    private int vibSwitchState = 1;
+    private int sfxSwitchState = 1;
+    public GameObject vibSwitchButton;
+    public GameObject vibSwitchBg;
+    public GameObject sfxSwitchButton;
+    public GameObject sfxSwitchBg;
+
+    public void VibrationSwitchButtonClicked()
+    {
+        vibSwitchButton.transform.DOLocalMoveX(-vibSwitchButton.transform.localPosition.x, 0.2f);
+        vibSwitchState = Math.Sign(-vibSwitchButton.transform.localPosition.x);
+        if (vibSwitchState == 1)
+        {
+            vibSwitchBg.GetComponent<Image>().color = new Color32(255, 0, 0, 255);
+        }
+        else if (vibSwitchState == -1)
+        {
+            vibSwitchBg.GetComponent<Image>().color = new Color32(0, 255, 0, 255);
+
+        }
+    }    public void SFXSwitchButtonClicked()
+    {
+        sfxSwitchButton.transform.DOLocalMoveX(-sfxSwitchButton.transform.localPosition.x, 0.2f);
+        sfxSwitchState = Math.Sign(-sfxSwitchButton.transform.localPosition.x);
+        if (sfxSwitchState == 1)
+        {
+            sfxSwitchBg.GetComponent<Image>().color = new Color32(255, 0, 0, 255);
+        }
+        else if (sfxSwitchState == -1)
+        {
+            sfxSwitchBg.GetComponent<Image>().color = new Color32(0, 255, 0, 255);
+
+        }
+    }
+
+}
